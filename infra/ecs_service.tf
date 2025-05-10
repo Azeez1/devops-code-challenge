3a532ae2a454fa931cc5240c6f85f6b5f8e5b162
Azeez1 <sabaazeez12@gmail.com>
2025-05-10 16:10:02 -0400

    Create ecs_service.tf

---
+++ infra/ecs_service.tf
@@ -0,0 +1,87 @@
+# Grab your account’s default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Grab all subnet IDs in that VPC
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_ecs_task_definition" "frontend" {
+  family                   = "devops-challenge-frontend"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name         = "frontend"
+      image        = "${aws_ecr_repository.frontend.repository_url}:latest"
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+
+resource "aws_ecs_service" "frontend" {
+  name            = "frontend-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.frontend.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = data.aws_subnet_ids.default.ids
+    assign_public_ip = true
+    # If you have a security group, add: security_groups = [aws_security_group.app_sg.id]
+  }
+}
+
+
+resource "aws_ecs_task_definition" "backend" {
+  family                   = "devops-challenge-backend"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "128"
+  memory                   = "256"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name         = "backend"
+      image        = "${aws_ecr_repository.backend.repository_url}:latest"
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+
+resource "aws_ecs_service" "backend" {
+  name            = "backend-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.backend.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = data.aws_subnet_ids.default.ids
+    assign_public_ip = false
+  }
+}
