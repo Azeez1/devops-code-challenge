83e7ee52046dbb3fb80d4bb0e5bd8b1155bb6ae9
Azeez1 <sabaazeez12@gmail.com>
2025-05-12 18:32:06 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { API_URL } from './config';
 
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080/';
+
 function App() {
   const [successMessage, setSuccessMessage] = useState();
   const [failureMessage, setFailureMessage] = useState();
