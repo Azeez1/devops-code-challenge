646bcd497702f1d21193dea2c9a470707a393f03
Azeez1 <sabaazeez12@gmail.com>
2025-05-13 17:21:29 -0400

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://44.192.74.201:8080';
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080'; 
+
+console.log('Frontend API_URL used:', API_URL); // For easier debugging in browser
